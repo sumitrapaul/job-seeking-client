be197027129dab2ea72201fca0870470651d0872
Sumitra Paul <sumitra.cse.just2020@gmail.com>
2024-01-06 21:14:10 +0600

    client name update

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,14 +1,23 @@
 /* eslint-disable react/prop-types */
 
-
-const Test = ({t}) => {
-    const { name, image } = t;
-    return (
-        <div>
-           <img src={image} alt="not found" />
-           <h3 className="font-bold">{name}</h3> 
+const Test = ({ t }) => {
+  const { name, image } = t;
+  return (
+    <div className="mt-8">
+      <div className="card card-compact w-full bg-base-100 shadow-xl">
+        <figure>
+          <img
+            src={image}
+            alt="Shoes"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title flex justify-center items-center">{name}</h2>
+         
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Test;
